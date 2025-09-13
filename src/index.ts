3503729cefb1712864c4d3c78effcea8dd1f9e1c
raufurislam <raufurislam@gmail.com>
2025-09-13 17:53:33 +0600

    Wrap module 48 hello-prisma

--- src/index.ts
+++ src/index.ts
@@ -3,64 +3,74 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  // const result = await prisma.user.create({
-  //   data: {
-  //     name: "Raufur4",
-  //     email: "raufur4@gmail.com",
-  //   },
-  // });
-  // console.log(result);
-  // const usersData = await prisma.user.findMany({
-  //   where: {
-  //     id: 3,
-  //     // name: "Raufur1",
-  //   },
-  // });
-  // const findUserById = await prisma.user.findFirstOrThrow({
-  //   where: {
-  //     id: 5,
-  //   },
-  // });
-  // console.log(findUserById);
-  // Update User data
-  // const updatedUser = await prisma.user.update({
-  //   where: {
-  //     id: 3,
-  //   },
-  //   data: {
-  //     name: "Raufur Islam 3",
-  //     email: "raufur3@gmail.com",
-  //   },
-  // });
-  // console.log(updatedUser);
-  // const updatedProfilePhoto = await prisma.user.updateManyAndReturn({
-  //   where: {
-  //     profilePhoto: "abcdefghijklmnopqrstuvwxyz",
-  //   },
-  //   data: {
-  //     profilePhoto: "https://ibb.co.com/x8JDbCbF",
-  //   },
-  // });
-  // console.log(updatedProfilePhoto);
-  // Delete
-  // const deleteUser = await prisma.user.delete({
-  //   where: {
-  //     id: 3,
-  //   },
-  // });
-  // console.log(deleteUser);
+  // Create/Insert data into DB
+  const result = await prisma.user.create({
+    data: {
+      name: "Raufur4",
+      email: "raufur4@gmail.com",
+    },
+  });
+  console.log(result);
 
-  const deleteUser = await prisma.user.deleteMany({
+  // Retrive all data from DB
+  const usersData = await prisma.user.findMany({
+    where: {
+      id: 3,
+      // name: "Raufur1",
+    },
+  });
+
+  // Find a unique user by ID
+  const findUserById = await prisma.user.findFirstOrThrow({
+    where: {
+      id: 5,
+    },
+  });
+  console.log(findUserById);
+
+  // Update user data by ID (only one user)
+  const updatedUser = await prisma.user.update({
+    where: {
+      id: 3,
+    },
+    data: {
+      name: "Raufur Islam 3",
+      email: "raufur3@gmail.com",
+    },
+  });
+  console.log(updatedUser);
+
+  // Update multiple users by condition
+  const updatedProfilePhoto = await prisma.user.updateManyAndReturn({
+    where: {
+      profilePhoto: "abcdefghijklmnopqrstuvwxyz",
+    },
+    data: {
+      profilePhoto: "https://ibb.co.com/x8JDbCbF",
+    },
+  });
+  console.log(updatedProfilePhoto);
+
+  // Delete single users by condition
+  const deleteUser = await prisma.user.delete({
+    where: {
+      id: 3,
+    },
+  });
+  console.log(deleteUser);
+
+  // Delete multiple users by condition
+  const deleteUsers = await prisma.user.deleteMany({
     where: {
       id: {
         gt: 2,
       },
     },
   });
-  console.log(deleteUser);
+  console.log(deleteUsers);
 
-  const usersData = await prisma.user.findMany();
-  console.log(usersData);
+  const AllUsersData = await prisma.user.findMany();
+  console.log(AllUsersData);
 }
 
 main();
