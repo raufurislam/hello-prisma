946a1273b6a55e3e81f73d1bd7767aed15728277
raufurislam <raufurislam@gmail.com>
2025-09-19 18:32:51 +0600

    Random

--- src/app/modules/users/users.service.ts
+++ src/app/modules/users/users.service.ts
@@ -1,8 +1,20 @@
 import type { Prisma } from "@prisma/client";
 import { prisma } from "../../config/db";
+import AppError from "../../errorHelpers/AppError";
+import httpStatus from "http-status-codes";
 
 const createUser = async (data: Prisma.UserCreateInput) => {
-  return await prisma.user.create({ data });
+  const isUserExist = await prisma.user.findUnique({
+    where: { email: data.email },
+  });
+
+  if (isUserExist) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User Already Exist");
+  }
+  const createUser = await prisma.user.create({
+    data,
+  });
+  return createUser;
 };
 
 export const UserServices = {
