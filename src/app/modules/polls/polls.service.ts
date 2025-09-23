e061162645b3ea0b7b86b599b9c796857405ab2b
raufurislam <raufurislam@gmail.com>
2025-09-24 01:02:24 +0600

    Final vote-war

--- src/app/modules/polls/polls.service.ts
+++ src/app/modules/polls/polls.service.ts
@@ -2,19 +2,41 @@ import { Option, Poll } from "@prisma/client";
 import { prisma } from "../../config/db";
 
 const createPoll = async (data: Poll & { options: Option }) => {
-  const { title, creatorId, options } = data;
+  // const { title, creatorId, options } = data;
 
+  // const optionsArray = Array.isArray(options) ? options : [];
+
+  // return await prisma.poll.create({
+  //   data: {
+  //     title,
+  //     creatorId,
+  //     options: {
+  //       create: optionsArray.map((text) => ({ text })),
+  //     },
+  //   },
+  //   include: { options: true },
+  // });
+
+  const { title, creatorId, options } = data;
   const optionsArray = Array.isArray(options) ? options : [];
 
-  return await prisma.poll.create({
-    data: {
-      title,
-      creatorId,
-      options: {
-        create: optionsArray.map((text) => ({ text })),
-      },
-    },
-    include: { options: true },
+  return await prisma.$transaction(async (tx) => {
+    // create poll
+    const poll = await tx.poll.create({ data: { title, creatorId } });
+
+    // create options
+    await tx.option.createMany({
+      data: optionsArray.map((text) => ({
+        text,
+        pollId: poll.id,
+      })),
+    });
+
+    // return
+    return await tx.poll.findUnique({
+      where: { id: poll.id },
+      include: { options: true },
+    });
   });
 };
 
